387cda1e9855fdecef7b95246e2dee8a1567d90d
asebaresllll <alfonso.sebares@estudiante.uam.es>
2016-11-18 00:37:16 +0100

    Porcentajes añadidos

--- P3/skriptllex.sh
+++ P3/skriptllex.sh
@@ -1,36 +1,42 @@
 #!bin/bash
 
 #============================================================================================
-#VARIABLES
+#DOCUMENTACION
+#============================================================================================
+#Sobre divisiones de resultados de otros comandos:
+#	(1)	http://unix.stackexchange.com/questions/24035/how-to-calculate-values-in-a-shell-script
+#	(2)	http://stackoverflow.com/questions/12147040/division-in-script-and-floating-point
+#
+#============================================================================================
+#EJCUCION
 #============================================================================================
 
-#============================================================================================
-#EEJCUCION
-#============================================================================================
-
-echo "\n============Porcentajes de paquetes============\n"
+echo "\n============Porcentajes de paquetes============"
 
 #TODOS los paquetes
-echo "\nVolcando todos los paquetes a fichero..."
+echo "Volcando todos los paquetes a fichero..."
 #tshark -r traza.pcap -T text -V > todos_paquetes.txt
-tshark -r traza.pcap -Y 'eth' >> todos_paquetes.out
+if [ ! -f todos_paquetes.out ]; then
+    tshark -r traza.pcap -Y 'eth' >> todos_paquetes.out
+fi
 
 #Paquetes que son ETH|VLAN|IP
-echo "\nFiltrando paquetes ETH|VLAN|IP..."
-tshark -r traza.pcap -Y 'eth.type==0x8100 && vlan.etype==0x0800' > eth_ip.out
-
+echo "Filtrando paquetes ETH|VLAN|IP..."
 #Paquetes que son ETH|IP
-echo "\nFiltrando paquetes que son ETH|IP..."
-tshark -r traza.pcap -Y 'eth.type==0x8100' >> eth_ip.out
+echo "Filtrando paquetes que son ETH|IP..."
+if [ ! -f eth_ip.out ]; then
+    tshark -r traza.pcap -Y 'eth.type==0x8100 && vlan.etype==0x0800' > eth_ip.out
+    tshark -r traza.pcap -Y 'eth.type==0x8100' >> eth_ip.out
+fi
 
 #Paquetes que NO son ETH|VLAN|IP
-echo "\nFiltrando paquetes que NO son ETH|VLAN|IP..."
-tshark -r traza.pcap -Y 'eth.type==0x8100 && !vlan.etype==0x0800' > eth_NO_ip.out
-
+echo "Filtrando paquetes que NO son ETH|VLAN|IP..."
 #Paquetes que NO son ETH|VLAN
-echo "\nFiltrando paquetes que NO son ETH|VLAN..."
-tshark -r traza.pcap -Y '!eth.type==0x8100' >> eth_NO_ip.out
-
+echo "Filtrando paquetes que NO son ETH|VLAN..."
+if [ ! -f eth_NO_ip.out ]; then
+    tshark -r traza.pcap -Y 'eth.type==0x8100 && !vlan.etype==0x0800' > eth_NO_ip.out
+    tshark -r traza.pcap -Y '!eth.type==0x8100' >> eth_NO_ip.out
+fi
 
 #count=$let ( ($count_todos + $count_eth_ip)/$count_todos))
 #porcentaje_IP=$((count_eth_ip/count_todos))
@@ -44,6 +50,10 @@ echo "$count_todos"
 echo "$count_eth_ip"
 echo "$count_eth_NO_ip"
 
-echo "\nPorcentaje paquetes IP:\n"
-echo "$((100*($count_eth_ip/$count_todos)))"
+echo "\nPorcentaje paquetes IP:"
+# $((100*($count_eth_ip/$count_todos)))
+#let a=count_eth_ip/count_todos
+div=$(echo "scale=2; $count_eth_ip/$count_todos" | bc)
+res=$(echo "scale=2; $div*100" | bc)
+echo "$res"%
 
